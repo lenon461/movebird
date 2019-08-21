9a2842e84cf5d218eb98d28940bc2e50c88d4ea6
jslee02 <jslee02@korens.co.kr>
2019-08-21 16:35:23 +0900

    :tada: initial commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import Vue from 'vue'
+import './plugins/bootstrap-vue'
+import App from './App'
+import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.config.productionTip = false
+ 
+Vue.use(BootstrapVue)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
---
+++ src/pages/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}wd</h1>
+    <glide>dd</glide>
+  </div>
+</template>
+
+<script>
+import GlideVue from '../components/Glide'
+
+export default {
+  name: 'HelloWorld',
+  components: {
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components : {
+    'Glide' : GlideVue,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
